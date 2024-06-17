b5a22753ac98e8c0d031272dcfb1990bed59afda
dilterporto <contato@dilterporto.com>
2024-06-17 09:24:17 -0300

    feat: add task repository implementation

---
+++ src/Tasks.Persistence/Repositories/TaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+using Tasks.Abstractions.EventSourcing;
+using Tasks.Domain.Aggregates.Tasks;
+using Tasks.Persistence.Events;
+
+namespace Tasks.Persistence.Repositories;
+
+public class TaskRepository(EventsDbContext dbContext, IEventCommiters eventCommiters, ILogger<ITaskRepository> logger) 
+  : Repository<TaskAggregate, ITaskRepository>(dbContext, eventCommiters, logger), ITaskRepository
+{
+
+}
