3e0751b016edbdc2ee2241ec23dd1ddc973e0a72
dilterporto <contato@dilterporto.com>
2024-06-17 09:23:53 -0300

    feat: add task repository interface

---
+++ src/Tasks.Domain/Aggregates/Tasks/ITaskRepository.cs
@@ -0,0 +1,9 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace Tasks.Domain.Aggregates.Tasks;
+
+public interface ITaskRepository
+{
+  Task<Maybe<TaskAggregate>> LoadByIdAsync(Guid id);
+  Task SaveAsync(TaskAggregate aggregate);
+}
