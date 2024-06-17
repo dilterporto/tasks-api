3a21af5853a82951dcbbbae7a5f21b69e70035e3
dilterporto <contato@dilterporto.com>
2024-06-17 15:07:36 -0300

    chore: change get task by id use to fetch from projections

--- src/Tasks.Application/UseCases/GetTaskById/GetTaskByIdQuery.Handler.cs
+++ src/Tasks.Application/UseCases/GetTaskById/GetTaskByIdQuery.Handler.cs
@@ -1,17 +1,24 @@
 ﻿using AutoMapper;
 using CSharpFunctionalExtensions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Tasks.Application.Contracts;
 using Tasks.Domain.Aggregates.Tasks;
+using Tasks.Persistence.Projections;
 
 namespace Tasks.Application.UseCases.GetTaskById;
 
-public class GetTaskByIdQueryHandler(ITaskRepository taskRepository, IMapper mapper) : IRequestHandler<GetTaskByIdQuery, Result<TaskResponse>>
+public class GetTaskByIdQueryHandler(ProjectionsDbContext projectionsDbContext, IMapper mapper) : IRequestHandler<GetTaskByIdQuery, Result<TaskResponse>>
 {
   public async Task<Result<TaskResponse>> Handle(GetTaskByIdQuery request, CancellationToken cancellationToken)
   {
-    var task = await taskRepository.LoadByIdAsync(request.TaskId);
-    return task.HasNoValue ?
-      Result.Failure<TaskResponse>("Task not found") : mapper.Map<TaskResponse>(task.Value.State);
+    var task = await projectionsDbContext
+      .Set<TaskProjection>()
+      .TagWith("GetTaskByIdQuery - TaskProjection")
+      .AsNoTracking()
+      .FirstOrDefaultAsync(x => x.Id == request.TaskId, cancellationToken: cancellationToken);
+    
+    return task == null ?
+      Result.Failure<TaskResponse>("Task not found") : mapper.Map<TaskResponse>(task);
   }
 }
