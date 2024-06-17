8b6fe1cefabf0f129ed3c444fece77f582af299b
dilterporto <contato@dilterporto.com>
2024-06-17 09:19:51 -0300

    chore: add unit of work behavior to deal with transactions at the application layer

---
+++ src/Tasks.Persistence/UnitOfWorkBehavior.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Tasks.Abstractions.CQRS;
+using Tasks.Persistence.Events;
+
+namespace Tasks.Persistence;
+
+public class UnitOfWorkBehavior<TRequest, TResponse>(EventsDbContext unitOfWork) : CommandPipelineBehavior<TRequest, TResponse>
+  where TRequest : IRequest<TResponse>
+{
+  public override async Task<TResponse> Handle(ICommand<TResponse> command, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+  {
+    var response = await next();
+    await unitOfWork.SaveChangesAsync(cancellationToken);
+    return response;
+  }
+}
